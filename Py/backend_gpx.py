48a2351feca59abf7de582eb1664df2f81e6af2b
Richard Johansson <ricerijoh@gmail.com>
2022-02-22 23:14:03 +0100

    updated backend_gpx and log

--- Py/backend_gpx.py
+++ Py/backend_gpx.py
@@ -6,6 +6,8 @@ Author: Richard Johansson
 # Imports
 import gpxpy
 import numpy as np
+import matplotlib.pyplot as plt
+from geopy import distance 
 """
 Is a Class the way to go? a lot of functions to do various
 things with multiple imported gpx files will be done and 
@@ -14,18 +16,35 @@ to work with objects will most likey be the way to go.
 Will also build a Django integration with the front end 
 and it could be a class of its own.
 """
-#-------------- Functions --------------#
-def get_lat_lon_ele(path):
-    # Open gpx file
-    gpx_file = open(path, mode='rt', encoding ='utf-8')
-    # Parse gpx file 
-    gpx = gpxpy.parse(gpx_file)
-    # Loop to collect lon, lat and ele
-    lle = []
-    for track in gpx.tracks:
-        for segment in track.segments:
-            for point in segment.points:
-                lle.append([point.latitude,
-                            point.longitude,
-                            point.elevation])
-    return lle
+#-------------- Classes --------------#
+class Gps:
+    def __init__(self, file):
+       self.file = file 
+
+    def __repr__(self):
+       return np.array(self) 
+         
+    def get_lat_lon_ele(self):
+        # Open gpx file
+        gpx_file = open(self.file, mode='rt', encoding ='utf-8')
+        # Parse gpx file 
+        gpx = gpxpy.parse(gpx_file)
+        # Loop to collect lon, lat and ele
+        lle = []
+        for track in gpx.tracks:
+            for segment in track.segments:
+                for point in segment.points: 
+                    lle.append([point.latitude,
+                                point.longitude,
+                                point.elevation])
+        return np.array(lle)
+
+        
+    def plotPoints(self):
+        lle = self.get_lat_lon_ele()
+        plt.plot(lle[:,0],
+                 lle[:,1], 'r.')
+
+
+file = Gps('data/Walk1.gpx')
+dis , ele = file.gps_distance_elevation()
